966eb046f989f788be014064128aee329303cc12
郑文祥 <zhengwx0821@gmail.com>
2024-04-15 16:06:07 +0800

    ref |>  语法

---
+++ code/entry/src/main/ets/common/utils/CallBackType.d.ts
@@ -0,0 +1,14 @@
+
+/**
+ * 声明文件学习: https://ts.xcatliu.com/basics/declaration-files.html
+ */
+
+declare var EmptyCallback: () => void
+declare var BoolCallback: (value: Boolean) => void
+
+// 防止命名冲突, 用 namespace 包裹一层
+declare namespace Homework{
+  interface Callback<T> {
+      (T): void
+  }
+}
